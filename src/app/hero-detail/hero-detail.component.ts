d6caaca00d0f9576d8d793345bbeb555c6b39af3
codemvs <codemvs@gmail.com>
2021-04-21 11:41:20 -0500

    Uso de ActivatedRoute muestra informacion de la ruta de instancia y Location servicio para interactuar con el navegador

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // Holds information about the route to this instance
+import { Location } from '@angular/common'; // is an Angular service for interacting with the browser
+
 import {Hero} from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -7,9 +12,22 @@ import {Hero} from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero?:Hero;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location    
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void{
+    const id = Number( this.route.snapshot.paramMap.get('id') );
+    this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+  }
+  goBack(): void{
+    this.location.back();
+  }
 }
